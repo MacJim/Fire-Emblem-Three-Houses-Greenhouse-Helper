2c0e55b8e508458f0b5b4fff17b01f8d87e44076
Jim Shi <shiyilin2008@gmail.com>
2020-12-27 15:21:19 -0800

    Adds seeds rank information.

---
+++ constants/seeds_info.py
@@ -0,0 +1,117 @@
+"""
+Seed information.
+
+Source: https://serenesforest.net/three-houses/monastery/greenhouse/
+"""
+
+# MARK: - Keys
+SEED_NAME_KEY = "name"
+SEED_RANK_KEY = "rank"    ; """This is a hidden value used to calculate the final yield."""
+SEED_GRADE_KEY = "grade"    ; """This is the visible \"star\" grade in the plant menu."""
+
+
+# MARK: - Seeds
+MIXED_HERB_SEEDS = {
+    SEED_NAME_KEY: "Mixed Herb Seeds",
+    SEED_RANK_KEY: 27,
+}
+
+WESTERN_FODLAN_SEEDS = {
+    SEED_NAME_KEY: "Western Fodlan Seeds",
+    SEED_RANK_KEY: 9,
+}
+
+ROOT_VEGETABLE_SEEDS = {
+    SEED_NAME_KEY: "Root Vegetable Seeds",
+    SEED_RANK_KEY: 49,
+}
+
+VEGETABLE_SEEDS = {
+    SEED_NAME_KEY: "Vegetable Seeds",
+    SEED_RANK_KEY: 33,
+}
+
+NORTHERN_FODLAN_SEEDS = {
+    SEED_NAME_KEY: "Northern Fodlan Seeds",
+    SEED_RANK_KEY: 53,
+}
+
+MORFIS_PLUM_SEEDS = {
+    SEED_NAME_KEY: "Morfis-Plum Seeds",
+    SEED_RANK_KEY: 18,
+}
+
+SOUTHERN_FODLAN_SEEDS = {
+    SEED_NAME_KEY: "Southern Fodlan Seeds",
+    SEED_RANK_KEY: 37,
+}
+
+MORFIS_SEEDS = {
+    SEED_NAME_KEY: "Morfis Seeds",
+    SEED_RANK_KEY: 23,
+}
+
+NORDSALAT_SEEDS = {
+    SEED_NAME_KEY: "Nordsalat Seeds",
+    SEED_RANK_KEY: 3,
+}
+
+BOA_FRUIT_SEEDS = {
+    SEED_NAME_KEY: "Boa-Fruit Seeds",
+    SEED_RANK_KEY: 31,
+}
+
+ALBINEAN_SEEDS = {
+    SEED_NAME_KEY: "Albinean Seeds",
+    SEED_RANK_KEY: 20,
+}
+
+EASTERN_FODLAN_SEEDS = {
+    SEED_NAME_KEY: "Eastern Fodlan Seeds",
+    SEED_RANK_KEY: 42,
+}
+
+ANGELICA_SEEDS = {
+    SEED_NAME_KEY: "Angelica Seeds",
+    SEED_RANK_KEY: 34,
+}
+
+MIXED_FRUIT_SEEDS = {
+    SEED_NAME_KEY: "Mixed Fruit Seeds",
+    SEED_RANK_KEY: 44,
+}
+
+RED_FLOWER_SEEDS = {
+    SEED_NAME_KEY: "Red Flower Seeds",
+    SEED_RANK_KEY: 24,
+}
+
+WHITE_FLOWER_SEEDS = {
+    SEED_NAME_KEY: "White Flower Seeds",
+    SEED_RANK_KEY: 5,
+}
+
+BLUE_FLOWER_SEEDS = {
+    SEED_NAME_KEY: "Blue Flower Seeds",
+    SEED_RANK_KEY: 38,
+}
+
+PURPLE_FLOWER_SEEDS = {
+    SEED_NAME_KEY: "Purple Flower Seeds",
+    SEED_RANK_KEY: 16,
+}
+
+YELLOW_FLOWER_SEEDS = {
+    SEED_NAME_KEY: "Yellow Flower Seeds",
+    SEED_RANK_KEY: 55,
+}
+
+GREEN_FLOWER_SEEDS = {
+    SEED_NAME_KEY: "Green Flower Seeds",
+    SEED_RANK_KEY: 10,
+}
+
+PALE_BLUE_FLOWER_SEEDS = {
+    SEED_NAME_KEY: "Pale-Blue Flower Seeds",
+    SEED_RANK_KEY: 1,
+}
