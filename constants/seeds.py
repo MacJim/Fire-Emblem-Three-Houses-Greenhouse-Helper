eeec2062c7cc08103e99df4238f7e2a69612a6eb
Jim Shi <shiyilin2008@gmail.com>
2020-12-27 16:35:58 -0800

    Adds most seed grade information.
    The remaining grades will be added when I obtain them in-game.

--- constants/seeds.py
+++ constants/seeds.py
@@ -15,104 +15,125 @@ SEED_GRADE_KEY = "grade"    ; """This is the visible \"star\" grade in the plant
 MIXED_HERB_SEEDS = {
     SEED_NAME_KEY: "Mixed Herb Seeds",
     SEED_RANK_KEY: 27,
+    SEED_GRADE_KEY: 1,
 }
 
 WESTERN_FODLAN_SEEDS = {
     SEED_NAME_KEY: "Western Fodlan Seeds",
     SEED_RANK_KEY: 9,
+    SEED_GRADE_KEY: 1,
 }
 
 ROOT_VEGETABLE_SEEDS = {
     SEED_NAME_KEY: "Root Vegetable Seeds",
     SEED_RANK_KEY: 49,
+    SEED_GRADE_KEY: 1,
 }
 
 VEGETABLE_SEEDS = {
     SEED_NAME_KEY: "Vegetable Seeds",
     SEED_RANK_KEY: 33,
+    SEED_GRADE_KEY: 1,
 }
 
 NORTHERN_FODLAN_SEEDS = {
     SEED_NAME_KEY: "Northern Fodlan Seeds",
     SEED_RANK_KEY: 53,
+    SEED_GRADE_KEY: 2,
 }
 
 MORFIS_PLUM_SEEDS = {
     SEED_NAME_KEY: "Morfis-Plum Seeds",
     SEED_RANK_KEY: 18,
+    SEED_GRADE_KEY: 0,    # TODO:
 }
 
 SOUTHERN_FODLAN_SEEDS = {
     SEED_NAME_KEY: "Southern Fodlan Seeds",
     SEED_RANK_KEY: 37,
+    SEED_GRADE_KEY: 2,
 }
 
 MORFIS_SEEDS = {
     SEED_NAME_KEY: "Morfis Seeds",
     SEED_RANK_KEY: 23,
+    SEED_GRADE_KEY: 2,
 }
 
 NORDSALAT_SEEDS = {
     SEED_NAME_KEY: "Nordsalat Seeds",
     SEED_RANK_KEY: 3,
+    SEED_GRADE_KEY: 4,
 }
 
 BOA_FRUIT_SEEDS = {
     SEED_NAME_KEY: "Boa-Fruit Seeds",
     SEED_RANK_KEY: 31,
+    SEED_GRADE_KEY: 0,    # TODO:
 }
 
 ALBINEAN_SEEDS = {
     SEED_NAME_KEY: "Albinean Seeds",
     SEED_RANK_KEY: 20,
+    SEED_GRADE_KEY: 0,    # TODO:
 }
 
 EASTERN_FODLAN_SEEDS = {
     SEED_NAME_KEY: "Eastern Fodlan Seeds",
     SEED_RANK_KEY: 42,
+    SEED_GRADE_KEY: 2,
 }
 
 ANGELICA_SEEDS = {
     SEED_NAME_KEY: "Angelica Seeds",
     SEED_RANK_KEY: 34,
+    SEED_GRADE_KEY: 0,    # TODO:
 }
 
 MIXED_FRUIT_SEEDS = {
     SEED_NAME_KEY: "Mixed Fruit Seeds",
     SEED_RANK_KEY: 44,
+    SEED_GRADE_KEY: 1,
 }
 
 RED_FLOWER_SEEDS = {
     SEED_NAME_KEY: "Red Flower Seeds",
     SEED_RANK_KEY: 24,
+    SEED_GRADE_KEY: 3,
 }
 
 WHITE_FLOWER_SEEDS = {
     SEED_NAME_KEY: "White Flower Seeds",
     SEED_RANK_KEY: 5,
+    SEED_GRADE_KEY: 3,
 }
 
 BLUE_FLOWER_SEEDS = {
     SEED_NAME_KEY: "Blue Flower Seeds",
     SEED_RANK_KEY: 38,
+    SEED_GRADE_KEY: 3,
 }
 
 PURPLE_FLOWER_SEEDS = {
     SEED_NAME_KEY: "Purple Flower Seeds",
     SEED_RANK_KEY: 16,
+    SEED_GRADE_KEY: 3,
 }
 
 YELLOW_FLOWER_SEEDS = {
     SEED_NAME_KEY: "Yellow Flower Seeds",
     SEED_RANK_KEY: 55,
+    SEED_GRADE_KEY: 3,
 }
 
 GREEN_FLOWER_SEEDS = {
     SEED_NAME_KEY: "Green Flower Seeds",
     SEED_RANK_KEY: 10,
+    SEED_GRADE_KEY: 3,
 }
 
 PALE_BLUE_FLOWER_SEEDS = {
     SEED_NAME_KEY: "Pale-Blue Flower Seeds",
     SEED_RANK_KEY: 1,
+    SEED_GRADE_KEY: 3,
 }
