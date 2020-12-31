30274efbfbc11fa62492a58b9429318974f429ce
Jim Shi <shiyilin2008@gmail.com>
2020-12-30 23:27:30 -0800

    Yield level score is now separated into base yield and cultivation yield scores.
    This makes it easier to calculate the minimum cultivation tier.

--- helpers/yield_level.py
+++ helpers/yield_level.py
@@ -7,12 +7,38 @@ import typing
 
 
 # MARK: - Yield level calculation
-def get_yield_level_score(ranks: typing.List[int], grades: typing.List[int], cultivation_tier: int) -> int:
+def get_base_yield_level_score(ranks: typing.List[int], grades: typing.List[int]) -> int:
+    """
+    Get the yield level score without any cultivation methods.
+
+    :param ranks: Seed ranks.
+    :param grades: Seed grades.
+    :return:
+    """
     rank_score = (12 - sum(ranks) % 12) * 5
     grade_score = math.floor(sum(grades) / 5 * 4)
+
+    return rank_score + grade_score
+
+
+def get_minimum_cultivation_tier(current_score: int, target_score: int) -> int:
+    if (current_score >= target_score):
+        return 0
+
+    deficit = target_score - current_score
+    cultivation_tier = math.ceil(deficit / 2 - 4)
+
+    return cultivation_tier
+
+
+def get_cultivation_yield_level_score(cultivation_tier: int) -> int:
     cultivation_score = (cultivation_tier + 4) * 2
 
-    return rank_score + grade_score + cultivation_score
+    return cultivation_score
+
+
+def get_yield_level_score(ranks: typing.List[int], grades: typing.List[int], cultivation_tier: int) -> int:
+    return get_base_yield_level_score(ranks, grades) + get_cultivation_yield_level_score(cultivation_tier)
 
 
 # MARK: - Yield level string
